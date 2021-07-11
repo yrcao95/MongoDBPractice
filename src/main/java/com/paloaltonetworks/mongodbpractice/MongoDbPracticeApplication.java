12aea261fea3fcf66bb32d5ce2c461a74f6b26ba
Yirui Cao <yiruicao@Yiruis-MacBook-Pro.local>
2021-07-11 00:57:22 -0500

    Init project

---
+++ src/main/java/com/paloaltonetworks/mongodbpractice/MongoDbPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.paloaltonetworks.mongodbpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoDbPracticeApplication.class, args);
+    }
+
+}
